d7ae23a27e0e087cab1b7a27dca540b6c3a7889b
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-06-30 21:30:43 +0530

    feat(router): add Dockerfile and .dockerignore; configure nodemon for development

--- sandbox/router/src/app.js
+++ sandbox/router/src/app.js
@@ -3,10 +3,19 @@ import morgan from "morgan";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
+app.use(morgan("combined"));
 
 app.use((req, res, next) => {
   const host = req.headers.host;
   const sandboxId = host.split(".")[0];
+
+  const target = `http://sandbox-service-${sandboxId}`;
+
+  return createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    ws: true,
+  })(req, res, next);
 });
 
 export default app;
