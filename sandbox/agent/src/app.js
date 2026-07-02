a928536319fb51e800b3779a06c6975e8019a20c
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-07-02 18:27:42 +0530

    feat(agent): add Dockerfile and .dockerignore, implement file listing endpoint

--- sandbox/agent/src/app.js
+++ sandbox/agent/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import morgan from "morgan";
 import fs from "fs";
 
+const WORKING_DIR = "/workspace";
+
 const app = express();
 
 app.use(morgan("dev"));
@@ -10,6 +12,15 @@ app.get("/", (req, res) => {
   res.status(200).json({
     message: "Hello from sandbox agent",
     status: "success",
+  });
+});
+
+app.get("/list-files", async (req, res) => {
+  const elements = await fs.promises.readdir(WORKING_DIR);
+
+  res.status(200).json({
+    message: "Elements in working directory",
+    elements,
   });
 });
 
