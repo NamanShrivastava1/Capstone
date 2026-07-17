f78c3758073d3f83eba666df65919d7302355000
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-07-17 17:04:21 +0530

    feat(notification): implement notification service with RabbitMQ integration and Kubernetes deployment

--- notification/src/email.js
+++ notification/src/email.js
@@ -20,7 +20,7 @@ transporter.verify((error, success) => {
 });
 
 // Function to send email
-const sendEmail = async (to, subject, text, html) => {
+export const sendEmail = async (to, subject, text, html) => {
   try {
     const info = await transporter.sendMail({
       from: `"Your Name" <${process.env.EMAIL_USER}>`, // sender address
--- notification/src/mq.js
+++ notification/src/mq.js
@@ -8,8 +8,4 @@ const channel = await connection.createChannel();
 
 channel.assertQueue(QUEUE, { durable: true });
 
-export async function sendAuthNotification(message) {
-  channel.sendToQueue(QUEUE, Buffer.from(JSON.stringify(message)), {
-    persistent: true,
-  });
-}
+export default channel;
