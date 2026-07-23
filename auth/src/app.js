c3609fc2c93dc6c3dec6f836e18fbfbea8d80859
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-07-23 23:35:41 +0530

    fix(cors): correct typo in CORS origin URL

--- auth/src/app.js
+++ auth/src/app.js
@@ -16,7 +16,7 @@ app.use(cookies());
 app.use(passport.initialize());
 app.use(
   cors({
-    origin: "htpp://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
   }),
 );
